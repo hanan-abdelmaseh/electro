0b4965b9dc87fa0377b920714a53be00ed12c784
hanan-abdelmaseh <hananabdelmaseh9@gmail.com>
2025-02-26 20:18:14 +0200

    adding navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ServiceDetailComponent } from './components/service-detail/service-detail.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ServiceDetailComponent,
+    ProjectsComponent,
+    ContactUsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
